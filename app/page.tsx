755169dac0e9f41ffb2a8e1d0106492b1cad2051
Debanjan Mondal <debanjanmondalk2005@gmail.com>
2024-09-16 12:16:34 +0530

    added pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,79 +1,31 @@
-import { cn } from "@/lib/utils";
-import React from "react";
-import { BentoGrid, BentoGridItem } from "@/components/ui/bento-grid";
-import {
-  IconArrowWaveRightUp,
-  IconBoxAlignRightFilled,
-  IconBoxAlignTopLeft,
-  IconClipboardCopy,
-  IconFileBroken,
-  IconSignature,
-  IconTableColumn,
-} from "@tabler/icons-react";
+import React from 'react';
+import Link from 'next/link';
 
-const BentoGridDemo =() =>{
+const Page = () => {
   return (
-    <BentoGrid className="max-w-4xl mx-auto">
-      {items.map((item, i) => (
-        <BentoGridItem
-          key={i}
-          title={item.title}
-          description={item.description}
-          header={item.header}
-          icon={item.icon}
-          className={i === 3 || i === 6 ? "md:col-span-2" : ""}
+    <div
+      className="flex justify-center items-center h-screen bg-cover bg-no-repeat"
+      style={{
+        // backgroundImage: `url("https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExZTMwYmR1NHQxcnA3M29jODUwN3V6cXgwa252Z2N5c25rdjRlb2dyZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/9bTjZrytydVRK/giphy.webp")`,
+      }}
+    >
+      <div className="flex flex-col space-y-2 items-center text-white">
+        <img
+          src="https://media1.tenor.com/m/BelPRtxDqxQAAAAC/spaceinvaders-videogames.gif"
+          alt="Space Invaders GIF"
+          className="w-[400px] h-auto"
         />
-      ))}
-    </BentoGrid>
+        
+        <Link href="/pages/home" passHref>
+          <img
+            src="https://media1.tenor.com/m/fcQE0xInZG8AAAAC/game-start.gif"
+            alt="Game Start GIF"
+            className="rounded-lg w-[280px] cursor-pointer"
+          />
+        </Link>
+      </div>
+    </div>
   );
-}
-const Skeleton = () => (
-  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
-);
-const items = [
-  {
-    title: "The Dawn of Innovation",
-    description: "Explore the birth of groundbreaking ideas and inventions.",
-    header: <Skeleton />,
-    icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
-  },
-  {
-    title: "The Digital Revolution",
-    description: "Dive into the transformative power of technology.",
-    header: <Skeleton />,
-    icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
-  },
-  {
-    title: "The Art of Design",
-    description: "Discover the beauty of thoughtful and functional design.",
-    header: <Skeleton />,
-    icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
-  },
-  {
-    title: "The Power of Communication",
-    description:
-      "Understand the impact of effective communication in our lives.",
-    header: <Skeleton />,
-    icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
-  },
-  {
-    title: "The Pursuit of Knowledge",
-    description: "Join the quest for understanding and enlightenment.",
-    header: <Skeleton />,
-    icon: <IconArrowWaveRightUp className="h-4 w-4 text-neutral-500" />,
-  },
-  {
-    title: "The Joy of Creation",
-    description: "Experience the thrill of bringing ideas to life.",
-    header: <Skeleton />,
-    icon: <IconBoxAlignTopLeft className="h-4 w-4 text-neutral-500" />,
-  },
-  {
-    title: "The Spirit of Adventure",
-    description: "Embark on exciting journeys and thrilling discoveries.",
-    header: <Skeleton />,
-    icon: <IconBoxAlignRightFilled className="h-4 w-4 text-neutral-500" />,
-  },
-];
+};
 
-export default BentoGridDemo;
+export default Page;
---
+++ app/pages/home/page.tsx
@@ -0,0 +1,16 @@
+
+import FloatingNavDemo from '@/components/Navbar/Navbaraccertinity'
+import PlaceholdersAndVanishInputDemo from '@/components/placehoderacer/placeholderacertinity'
+
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className=''>
+      <FloatingNavDemo/>
+      
+    </div>
+  )
+}
+
+export default page
