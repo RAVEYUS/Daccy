4f3402294b54706f06871c61785638ff9c643315
RAVEYUS <gtavcs35@gmail.com>
2024-10-03 19:13:09 +0530

    even more changes

--- components/ui/placeholders-and-vanish-input.tsx
+++ components/ui/placeholders-and-vanish-input.tsx
@@ -70,9 +70,9 @@ export function PlaceholdersAndVanishInput({
     const newData: any[] = [];
 
     for (let t = 0; t < 800; t++) {
-      let i = 4 * t * 800;
+      const i = 4 * t * 800;
       for (let n = 0; n < 800; n++) {
-        let e = i + 4 * n;
+        const e = i + 4 * n;
         if (
           pixelData[e] !== 0 &&
           pixelData[e + 1] !== 0 &&
