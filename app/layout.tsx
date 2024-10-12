bcda33706e13c5691397c16ed73667cc4e138d6b
Debanjan Mondal <debanjanmondalk2005@gmail.com>
2024-10-12 17:15:39 +0530

    fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,22 @@
-import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
-import './globals.css';
-import localFont from 'next/font/local';
-import { ThemeProvider } from '@/components/ui/themeprovider';
-
-// Import necessary styles for Clerk components
-import { Button } from '@/components/ui/button'; // Import your button styling
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import { ThemeProvider } from "@/components/ui/themeprovider";
 
 const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
 });
-
 const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
 });
 
-export const metadata = {
-  title: 'Daccy',
-  description: 'Learn coding efficiently',
+export const metadata: Metadata = {
+  title: "Daccy",
+  description: "Learn coding efficiently",
 };
 
 export default function RootLayout({
@@ -29,33 +25,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-          <ThemeProvider
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ThemeProvider
             attribute="class"
             defaultTheme="system"
             enableSystem
             disableTransitionOnChange
           >
             {children}
-            {/* Customizing Clerk components */}
-            <SignedIn>
-              <UserButton 
-                // Apply UI styles or classes to UserButton
-                
-              />
-            </SignedIn>
-            <SignedOut>
-              <SignInButton>
-                <Button variant="default">
-                  Login / Signup
-                </Button>
-              </SignInButton>
-            </SignedOut>
           </ThemeProvider>
-        </body>
-      </html>
-    </ClerkProvider>
+      </body>
+    </html>
   );
 }
