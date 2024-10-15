4a565fca306d0994c753e942a24dabe0086bdccb
RAVEYUS <gtavcs35@gmail.com>
2024-10-15 20:20:15 +0530

    changes to graph

--- app/pages/graph/page.tsx
+++ app/pages/graph/page.tsx
@@ -1,16 +1,29 @@
-"use client";
+// pages/tree.js
+"use client"
+import ForceDirectedTree from '@/components/loadGraph';
 
-import { useEffect } from "react";
-import { loadGraph } from "@/components/loadGraph";
+const TreePage = () => {
+  const nodes = [
+    { id: 'A', group: 1 },
+    { id: 'B', group: 1 },
+    { id: 'C', group: 2 },
+    { id: 'D', group: 2 },
+    { id: 'E', group: 3 },
+  ];
 
-export default function Home() {
-  useEffect(() => {
-    loadGraph();
-  }, []);
+  const links = [
+    { source: 'A', target: 'B' },
+    { source: 'A', target: 'C' },
+    { source: 'B', target: 'D' },
+    { source: 'C', target: 'E' },
+  ];
 
   return (
-    <div className="container">
-      <svg id="container" width="1060" height="960" viewBox="0 0 1060 960" preserveAspectRatio="xMidYMid meet"></svg>
+    <div>
+      <h1>Force-Directed Tree</h1>
+      <ForceDirectedTree nodes={nodes} links={links} width={800} height={600} />
     </div>
   );
-}
+};
+
+export default TreePage;
